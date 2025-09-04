1dd5dc65d8750febf715e6eb485cebedd5379c86
Sanaj Samarakoon <sanaj.samarakoon@gmail.com>
2025-09-04 10:54:06 +0530

    Initial Flask portfolio setup

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
 # Main Application File
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/projects/<project_id>')
+def project_detail(project_id):
+    return render_template('project_detail.html', project_id=project_id)
+
+if __name__ == '__main__':
+    app.run(debug=True)
